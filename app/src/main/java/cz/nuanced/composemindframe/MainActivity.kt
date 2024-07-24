990f6c3eb5894a14aad8266e9983a41d6fdd2d09
Jan Vaníček <ye@email.cz>
2024-07-24 15:59:02 +0200

    first concept of layout

--- app/src/main/java/cz/nuanced/composemindframe/MainActivity.kt
+++ app/src/main/java/cz/nuanced/composemindframe/MainActivity.kt
@@ -4,22 +4,46 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.withRunningRecomposer
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,22 +55,84 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeMindFrameTheme {
-                Scaffold(modifier = Modifier
-                    .fillMaxSize()
+                var text by remember { mutableStateOf("input text") }
+                Scaffold(
+//                    floatingActionButtonPosition = FabPosition.End,
+//                    floatingActionButton = {
+//                        FloatingActionButton(onClick = { /*TODO*/ }) {
+//                            ConfirmButton(onClick = { /*TODO*/ })
+//                        }
+//                    }
                 ) { innerPadding ->
-                    Column(modifier = Modifier
-                        .fillMaxSize()
-                        .padding(innerPadding)
-                        .background(Color.DarkGray)
-                    ){
-                        Meme(content = "Android")
-                        Meme(content = "Compose")
-                        Meme(content = "Often these building blocks are all you need. You can write" +
-                                " your own composable function to combine these layouts into a more" +
-                                " elaborate layout that suits your app.")
-                        ConfirmButton(onClick = { /*TODO*/ })
+                    Column(
+                        modifier = Modifier
+                            .background(Color.DarkGray)
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.Bottom
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .weight(3.6f)
+                                .background(Color.DarkGray)
+                                .verticalScroll(rememberScrollState()),
+                            verticalArrangement = Arrangement.Bottom
+                        ) {
+                            Meme(content = "Android")
+                            Meme(content = "Compose")
+                            repeat(12) {
+                                Meme(
+                                    content = "Often these building blocks are all you need. You can write" +
+                                            " your own composable function to combine these layouts into a more" +
+                                            " elaborate layout that suits your app."
+                                )
+                            }
+                        }
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(.4f)
+                                .padding(8.dp, 4.dp)
+                                .background(Color.DarkGray, shape = MaterialTheme.shapes.small)
+                                .horizontalScroll(rememberScrollState()),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            repeat(7) {
+                                TagButton(tag = "Android")
+                            }
+                        }
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
+                                .padding(8.dp, 0.dp)
+                                .background(Color.LightGray, shape = MaterialTheme.shapes.small)
+                        ) {
+                            BasicTextField(
+                                value = text,
+                                onValueChange = { text = it },
+                                textStyle = TextStyle(fontSize = 18.sp, color = Color.DarkGray),
+                                modifier = Modifier
+                                    .background(Color.LightGray, shape = MaterialTheme.shapes.small)
+                                    .padding(8.dp)
+                            )
+                            Button(
+                                onClick = { /*TODO*/ },
+                                modifier = Modifier
+                                    .align(Alignment.BottomEnd)
+                                    .padding(8.dp)
+                            ) {
+                                Text(text = "Send")
+                            }
+                            Button(
+                                onClick = { /*TODO*/ },
+                                modifier = Modifier
+                                    .align(Alignment.BottomStart)
+                                    .padding(8.dp)
+                            ) {
+                                Text(text = "Add")
+                            }
+                        }
                     }
-
 
                 }
             }
@@ -56,16 +142,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Meme(content: String) {
-    Text(
-        text = content,
+    Box(
         modifier = Modifier
-            .padding(4.dp, 6.dp)
+            .padding(8.dp)
             .fillMaxWidth()
-            .border(width = 1.dp, color = Color.Black)
-            .background(Color.Gray)
-        ,
-        color = Color.White
-    )
+            .background(Color.Gray, shape = MaterialTheme.shapes.small)
+    ) {
+        Text(
+            text = content,
+            modifier = Modifier
+                .padding(6.dp),
+            color = Color.White
+        )
+    }
 }
 
 @Composable
@@ -81,7 +170,8 @@ fun ConfirmButton(onClick: () -> Unit) {
             ),
         shape = RoundedCornerShape(40.dp)
     ) {
-        Text(text = "->",
+        Text(
+            text = "Confirm",
             color = Color.White,
             fontSize = 30.sp,
             modifier = Modifier
@@ -91,10 +181,35 @@ fun ConfirmButton(onClick: () -> Unit) {
     }
 }
 
+@Composable
+fun TagButton(tag: String) {
+    Button(
+        onClick = { /*TODO*/ },
+        Modifier
+            .padding(3.dp, 0.dp)
+    ) {
+        Text(
+            text = tag,
+            color = Color.White,
+            modifier = Modifier
+                .padding(2.dp)
+        )
+    }
+}
+
+@Composable
+fun Survey(modifier: Modifier = Modifier) {
+    Row {
+        Image(painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "logo")
+        Text(text = "logo", color = Color(0xFF7C1C1C))
+        RadioButton(selected = false, onClick = { /*TODO*/ })
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun MemePreview() {
     ComposeMindFrameTheme {
-        Meme("Android")
+        Survey()
     }
 }
